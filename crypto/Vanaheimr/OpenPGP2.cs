434108c393a690a724061f4876b46b365693ed22
Achim Friedland <github@ahzf.de>
2016-04-08 23:36:42 +0200

    Minor updates

--- crypto/Vanaheimr/OpenPGP2.cs
+++ crypto/Vanaheimr/OpenPGP2.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (c) 2014-2016, Achim 'ahzf' Friedland <achim@graphdefined.org>
+ * Copyright (c) 2014-2016, Achim 'ahzf' Friedland <achim.friedland@graphdefined.com>
  * This file is part of Vanaheimr BouncyCastle <http://www.github.com/Vanaheimr/BouncyCastle>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
