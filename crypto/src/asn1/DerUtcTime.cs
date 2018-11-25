362f0503271b8967d620df204704981ec603edc9
Achim Friedland <github@ahzf.de>
2018-11-25 02:17:10 +0100

    Minor refactorings

--- crypto/src/asn1/DerUtcTime.cs
+++ crypto/src/asn1/DerUtcTime.cs
@@ -6,258 +6,237 @@ using Org.BouncyCastle.Utilities;
 
 namespace Org.BouncyCastle.Asn1
 {
-    /**
-     * UTC time object.
-     */
-    public class DerUtcTime
-        : Asn1Object
-    {
-        private readonly string time;
 
-		/**
-         * return an UTC Time from the passed in object.
-         *
-         * @exception ArgumentException if the object cannot be converted.
-         */
-        public static DerUtcTime GetInstance(
-            object obj)
+    /// <summary>
+    /// UTC time object.
+    /// </summary>
+    public class DerUtcTime : Asn1Object
+    {
+
+        private readonly String time;
+
+
+        /// <summary>
+        /// The correct format for this is YYMMDDHHMMSSZ (it used to be that seconds were
+        /// never encoded. When you're creating one of these objects from scratch, that's
+        /// what you want to use, otherwise we'll try to deal with whatever Gets read from
+        /// the input stream... (this is why the input format is different from the GetTime()
+        /// method output).
+        /// </summary>
+        /// <param name="time">The time string.</param>
+        public DerUtcTime(String time)
         {
-            if (obj == null || obj is DerUtcTime)
+
+            this.time = time ?? throw new ArgumentNullException("time");
+
+            try
             {
-                return (DerUtcTime)obj;
+                ToDateTime();
+            }
+            catch (FormatException e)
+            {
+                throw new ArgumentException("invalid date string: " + e.Message);
             }
 
-            throw new ArgumentException("illegal object in GetInstance: " + obj.GetType().Name);
         }
 
-        /**
-         * return an UTC Time from a tagged object.
-         *
-         * @param obj the tagged object holding the object we want
-         * @param explicitly true if the object is meant to be explicitly
-         *              tagged false otherwise.
-         * @exception ArgumentException if the tagged object cannot
-         *               be converted.
-         */
-        public static DerUtcTime GetInstance(
-            Asn1TaggedObject	obj,
-            bool				isExplicit)
-        {
-			Asn1Object o = obj.GetObject();
-
-			if (isExplicit || o is DerUtcTime)
-			{
-				return GetInstance(o);
-			}
-
-			return new DerUtcTime(((Asn1OctetString)o).GetOctets());
-        }
-
-        /**
-         * The correct format for this is YYMMDDHHMMSSZ (it used to be that seconds were
-         * never encoded. When you're creating one of these objects from scratch, that's
-         * what you want to use, otherwise we'll try to deal with whatever Gets read from
-         * the input stream... (this is why the input format is different from the GetTime()
-         * method output).
-         * <p>
-         * @param time the time string.</p>
-         */
-        public DerUtcTime(
-            string time)
-        {
-			if (time == null)
-				throw new ArgumentNullException("time");
-
-			this.time = time;
-
-			try
-			{
-				ToDateTime();
-			}
-			catch (FormatException e)
-			{
-				throw new ArgumentException("invalid date string: " + e.Message);
-			}
-        }
-
-		/**
-         * base constructor from a DateTime object
-         */
-        public DerUtcTime(
-            DateTime time)
+        /// <summary>
+        /// Base constructor from a DateTime object.
+        /// </summary>
+        /// <param name="time"></param>
+        public DerUtcTime(DateTime time)
         {
             this.time = time.ToString("yyMMddHHmmss") + "Z";
         }
 
-		internal DerUtcTime(
-            byte[] bytes)
+        internal DerUtcTime(Byte[] bytes)
         {
-            //
+
             // explicitly convert to characters
-            //
             this.time = Strings.FromAsciiByteArray(bytes);
+
         }
 
-//		public DateTime ToDateTime()
-//		{
-//			string tm = this.AdjustedTimeString;
-//
-//			return new DateTime(
-//				Int16.Parse(tm.Substring(0, 4)),
-//				Int16.Parse(tm.Substring(4, 2)),
-//				Int16.Parse(tm.Substring(6, 2)),
-//				Int16.Parse(tm.Substring(8, 2)),
-//				Int16.Parse(tm.Substring(10, 2)),
-//				Int16.Parse(tm.Substring(12, 2)));
-//		}
+        //        public DateTime ToDateTime()
+        //        {
+        //            string tm = this.AdjustedTimeString;
+        //
+        //            return new DateTime(
+        //                Int16.Parse(tm.Substring(0, 4)),
+        //                Int16.Parse(tm.Substring(4, 2)),
+        //                Int16.Parse(tm.Substring(6, 2)),
+        //                Int16.Parse(tm.Substring(8, 2)),
+        //                Int16.Parse(tm.Substring(10, 2)),
+        //                Int16.Parse(tm.Substring(12, 2)));
+        //        }
 
-		/**
-		 * return the time as a date based on whatever a 2 digit year will return. For
-		 * standardised processing use ToAdjustedDateTime().
-		 *
-		 * @return the resulting date
-		 * @exception ParseException if the date string cannot be parsed.
-		 */
-		public DateTime ToDateTime()
-		{
-			return ParseDateString(TimeString, @"yyMMddHHmmss'GMT'zzz");
-		}
 
-		/**
-		* return the time as an adjusted date
-		* in the range of 1950 - 2049.
-		*
-		* @return a date in the range of 1950 to 2049.
-		* @exception ParseException if the date string cannot be parsed.
-		*/
-		public DateTime ToAdjustedDateTime()
-		{
-			return ParseDateString(AdjustedTimeString, @"yyyyMMddHHmmss'GMT'zzz");
-		}
 
-		private DateTime ParseDateString(
-			string	dateStr,
-			string	formatStr)
-		{
-			DateTime dt = DateTime.ParseExact(
-				dateStr,
-				formatStr,
-				DateTimeFormatInfo.InvariantInfo);
 
-			return dt.ToUniversalTime();
-		}
 
-		/**
-         * return the time - always in the form of
-         *  YYMMDDhhmmssGMT(+hh:mm|-hh:mm).
-         * <p>
-         * Normally in a certificate we would expect "Z" rather than "GMT",
-         * however adding the "GMT" means we can just use:
-         * <pre>
-         *     dateF = new SimpleDateFormat("yyMMddHHmmssz");
-         * </pre>
-         * To read in the time and Get a date which is compatible with our local
-         * time zone.</p>
-         * <p>
-         * <b>Note:</b> In some cases, due to the local date processing, this
-         * may lead to unexpected results. If you want to stick the normal
-         * convention of 1950 to 2049 use the GetAdjustedTime() method.</p>
-         */
-        public string TimeString
+
+
+
+        /// <summary>
+        /// Return an UTC Time from the passed in object.
+        /// </summary>
+        /// <param name="obj">An object.</param>
+        public static DerUtcTime GetInstance(Object obj)
         {
-			get
-			{
-				//
-				// standardise the format.
-				//
-				if (time.IndexOf('-') < 0 && time.IndexOf('+') < 0)
-				{
-					if (time.Length == 11)
-					{
-						return time.Substring(0, 10) + "00GMT+00:00";
-					}
-					else
-					{
-						return time.Substring(0, 12) + "GMT+00:00";
-					}
-				}
-				else
-				{
-					int index = time.IndexOf('-');
-					if (index < 0)
-					{
-						index = time.IndexOf('+');
-					}
-					string d = time;
 
-					if (index == time.Length - 3)
-					{
-						d += "00";
-					}
+            if (obj == null || obj is DerUtcTime)
+                return (DerUtcTime)obj;
 
-					if (index == 10)
-					{
-						return d.Substring(0, 10) + "00GMT" + d.Substring(10, 3) + ":" + d.Substring(13, 2);
-					}
-					else
-					{
-						return d.Substring(0, 12) + "GMT" + d.Substring(12, 3) + ":" +  d.Substring(15, 2);
-					}
-				}
-			}
+            throw new ArgumentException("Illegal object in GetInstance: " + obj.GetType().Name);
+
         }
 
-		[Obsolete("Use 'AdjustedTimeString' property instead")]
-		public string AdjustedTime
-		{
-			get { return AdjustedTimeString; }
-		}
-
-		/// <summary>
-		/// Return a time string as an adjusted date with a 4 digit year.
-		/// This goes in the range of 1950 - 2049.
-		/// </summary>
-		public string AdjustedTimeString
-		{
-			get
-			{
-				string d = TimeString;
-				string c = d[0] < '5' ? "20" : "19";
-
-				return c + d;
-			}
-		}
-
-        private byte[] GetOctets()
+        /// <summary>
+        /// Return an UTC Time from a tagged object.
+        /// </summary>
+        /// <param name="obj">The tagged object holding the object we want.</param>
+        /// <param name="isExplicit">Explicitly true if the object is meant to be explicitly tagged, false otherwise.</param>
+        public static DerUtcTime GetInstance(Asn1TaggedObject obj,
+                                             Boolean isExplicit)
         {
-            return Strings.ToAsciiByteArray(time);
+
+            var o = obj.GetObject();
+
+            if (isExplicit || o is DerUtcTime)
+                return GetInstance(o);
+
+            return new DerUtcTime(((Asn1OctetString)o).GetOctets());
+
         }
 
-		internal override void Encode(
-            DerOutputStream derOut)
+        /// <summary>
+        /// Return the time as a date based on whatever a 2 digit year will return. For
+        /// standardised processing use ToAdjustedDateTime().
+        /// </summary>
+        /// <returns></returns>
+        public DateTime ToDateTime()
+            => ParseDateString(TimeString, @"yyMMddHHmmss'GMT'zzz");
+
+        /// <summary>
+        /// Return the time as an adjusted date in the range of 1950 - 2049.
+        /// </summary>
+        /// <returns></returns>
+        public DateTime ToAdjustedDateTime()
+            => ParseDateString(AdjustedTimeString, @"yyyyMMddHHmmss'GMT'zzz");
+
+
+        private DateTime ParseDateString(String  dateStr,
+                                         String  formatStr)
+        {
+
+            var dt = DateTime.ParseExact(dateStr,
+                                         formatStr,
+                                         DateTimeFormatInfo.InvariantInfo);
+
+            return dt.ToUniversalTime();
+
+        }
+
+        /// <summary>
+        /// Return the time - always in the form of YYMMDDhhmmssGMT(+hh:mm|-hh:mm).
+        /// </summary>
+        /// <remarks>
+        /// <p>
+        /// Normally in a certificate we would expect "Z" rather than "GMT",
+        /// however adding the "GMT" means we can just use:
+        /// <pre>
+        ///     dateF = new SimpleDateFormat("yyMMddHHmmssz");
+        /// </pre>
+        /// To read in the time and Get a date which is compatible with our local
+        /// time zone.</p>
+        /// <p>
+        /// <b>Note:</b> In some cases, due to the local date processing, this
+        /// may lead to unexpected results. If you want to stick the normal
+        /// convention of 1950 to 2049 use the GetAdjustedTime() method.</p>
+        /// </remarks>
+        public String TimeString
+        {
+            get
+            {
+
+                // standardise the format.
+                if (time.IndexOf('-') < 0 && time.IndexOf('+') < 0)
+                {
+
+                    if (time.Length == 11)
+                        return time.Substring(0, 10) + "00GMT+00:00";
+
+                    else
+                        return time.Substring(0, 12) + "GMT+00:00";
+
+                }
+                else
+                {
+                    int index = time.IndexOf('-');
+                    if (index < 0)
+                        index = time.IndexOf('+');
+
+                    string d = time;
+
+                    if (index == time.Length - 3)
+                        d += "00";
+
+                    if (index == 10)
+                        return d.Substring(0, 10) + "00GMT" + d.Substring(10, 3) + ":" + d.Substring(13, 2);
+
+                    else
+                        return d.Substring(0, 12) + "GMT" + d.Substring(12, 3) + ":" +  d.Substring(15, 2);
+
+                }
+
+            }
+        }
+
+        [Obsolete("Use 'AdjustedTimeString' property instead")]
+        public String AdjustedTime
+            => AdjustedTimeString;
+
+        /// <summary>
+        /// Return a time string as an adjusted date with a 4 digit year.
+        /// This goes in the range of 1950 - 2049.
+        /// </summary>
+        public String AdjustedTimeString
+        {
+            get
+            {
+
+                String d = TimeString;
+                String c = d[0] < '5' ? "20" : "19";
+
+                return c + d;
+
+            }
+        }
+
+        private Byte[] GetOctets()
+            => Strings.ToAsciiByteArray(time);
+
+        internal override void Encode(DerOutputStream derOut)
         {
             derOut.WriteEncoded(Asn1Tags.UtcTime, GetOctets());
         }
 
-		protected override bool Asn1Equals(
-			Asn1Object asn1Object)
-		{
-			DerUtcTime other = asn1Object as DerUtcTime;
+        protected override Boolean Asn1Equals(Asn1Object asn1Object)
+        {
 
-			if (other == null)
-				return false;
+            if (!(asn1Object is DerUtcTime derUtcTime))
+                return false;
 
-			return this.time.Equals(other.time);
+            return time.Equals(derUtcTime?.time);
+
         }
 
-		protected override int Asn1GetHashCode()
-		{
-            return time.GetHashCode();
-        }
+        protected override Int32 Asn1GetHashCode()
+            => time.GetHashCode();
 
-		public override string ToString()
-		{
-			return time;
-		}
-	}
+        public override String ToString()
+            => time;
+
+    }
+
 }
--- crypto/src/asn1/x509/PrivateKeyUsagePeriod.cs
+++ crypto/src/asn1/x509/PrivateKeyUsagePeriod.cs
@@ -2,81 +2,72 @@ using System;
 
 namespace Org.BouncyCastle.Asn1.X509
 {
-	/// <remarks>
-	/// <pre>
-	/// PrivateKeyUsagePeriod ::= SEQUENCE
-	/// {
-	/// notBefore       [0]     GeneralizedTime OPTIONAL,
-	/// notAfter        [1]     GeneralizedTime OPTIONAL }
-	/// </pre>
-	/// </remarks>
-	public class PrivateKeyUsagePeriod
-		: Asn1Encodable
-	{
-		public static PrivateKeyUsagePeriod GetInstance(
-			object obj)
-		{
-			if (obj is PrivateKeyUsagePeriod)
-			{
-				return (PrivateKeyUsagePeriod) obj;
-			}
 
-			if (obj is Asn1Sequence)
-			{
-				return new PrivateKeyUsagePeriod((Asn1Sequence) obj);
-			}
+    /// <summary>
+    /// PrivateKeyUsagePeriod
+    /// </summary>
+    /// <remarks>
+    /// PrivateKeyUsagePeriod ::= SEQUENCE
+    /// {
+    ///     notBefore       [0]     GeneralizedTime OPTIONAL,
+    ///     notAfter        [1]     GeneralizedTime OPTIONAL
+    /// }
+    /// </remarks>
+    public class PrivateKeyUsagePeriod : Asn1Encodable
+    {
 
-			if (obj is X509Extension)
-			{
-				return GetInstance(X509Extension.ConvertValueToObject((X509Extension) obj));
-			}
+        public DerGeneralizedTime  NotBefore    { get; }
 
-			throw new ArgumentException("unknown object in GetInstance: " + obj.GetType().FullName, "obj");
-		}
+        public DerGeneralizedTime  NotAfter     { get; }
 
-		private DerGeneralizedTime _notBefore, _notAfter;
 
-		private PrivateKeyUsagePeriod(
-			Asn1Sequence seq)
-		{
-			foreach (Asn1TaggedObject tObj in seq)
-			{
-				if (tObj.TagNo == 0)
-				{
-					_notBefore = DerGeneralizedTime.GetInstance(tObj, false);
-				}
-				else if (tObj.TagNo == 1)
-				{
-					_notAfter = DerGeneralizedTime.GetInstance(tObj, false);
-				}
-			}
-		}
+        private PrivateKeyUsagePeriod(Asn1Sequence seq)
+        {
 
-		public DerGeneralizedTime NotBefore
-		{
-			get { return _notBefore; }
-		}
+            foreach (Asn1TaggedObject tObj in seq)
+            {
 
-		public DerGeneralizedTime NotAfter
-		{
-			get { return _notAfter; }
-		}
+                if (tObj.TagNo == 0)
+                    NotBefore = DerGeneralizedTime.GetInstance(tObj, false);
 
-		public override Asn1Object ToAsn1Object()
-		{
-			Asn1EncodableVector v = new Asn1EncodableVector();
+                else if (tObj.TagNo == 1)
+                    NotAfter  = DerGeneralizedTime.GetInstance(tObj, false);
 
-			if (_notBefore != null)
-			{
-				v.Add(new DerTaggedObject(false, 0, _notBefore));
-			}
+            }
 
-			if (_notAfter != null)
-			{
-				v.Add(new DerTaggedObject(false, 1, _notAfter));
-			}
+        }
 
-			return new DerSequence(v);
-		}
-	}
+        public static PrivateKeyUsagePeriod GetInstance(Object obj)
+        {
+
+            if (obj is PrivateKeyUsagePeriod)
+                return (PrivateKeyUsagePeriod) obj;
+
+            if (obj is Asn1Sequence)
+                return new PrivateKeyUsagePeriod((Asn1Sequence) obj);
+
+            if (obj is X509Extension)
+                return GetInstance(X509Extension.ConvertValueToObject((X509Extension) obj));
+
+            throw new ArgumentException("unknown object in GetInstance: " + obj.GetType().FullName, "obj");
+
+        }
+
+        public override Asn1Object ToAsn1Object()
+        {
+
+            var v = new Asn1EncodableVector();
+
+            if (NotBefore != null)
+                v.Add(new DerTaggedObject(false, 0, NotBefore));
+
+            if (NotAfter != null)
+                v.Add(new DerTaggedObject(false, 1, NotAfter));
+
+            return new DerSequence(v);
+
+        }
+
+    }
+
 }
